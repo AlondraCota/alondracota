badd3f8ff4d4eaafc478352b75afaec6ffd55d70
AlondraCota <90006992+AlondraCota@users.noreply.github.com>
2021-10-01 15:46:09 -0500

    Add files via upload

---
+++ array1.js
@@ -0,0 +1,3 @@
+var jeaney = [];
+var jeaney2 = ["omega","alfa",8,95,true];
+console.log(jeaney2[0,3]);
---
+++ arregloasociativo1.js
@@ -0,0 +1,37 @@
+    "nombre": "alondra",
+    "edad" : 19,
+    "casada" : false,
+    "informacion": {
+        "genero": {
+           "opera": false,  
+           "rap": false,  
+           "pop": true, 
+           "reggaeton": false, 
+        },
+        "pasa tiempos": {
+             "leer mangas": ["romance","reencanacion","apocaliptico", "aventura","gore"],
+             "ver dramas": ["vicenzo","true beauty","love alarm", "neverthele"],
+        },
+        "comida": {
+            "pansa": false,
+            "chilaquiles": true,
+            "tostadas de pata": false, 
+            "enmoladas": true, 
+            "hamburguesas": true,
+            "pollo rostiusado": false
+        },
+        "mascota": {
+            "perro": true,
+            "gato": false,
+            "pez": false,
+            "pajaros": true
+        },
+        "amistades": {
+            "dayan": true,
+            "bere": true,
+            "zoar": true,
+            "noe": true,
+        }
+    }
+}
+console.log(jeaney3);
